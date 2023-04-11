dba88c69d9d3820ba8fdf398a0a6489675d0c470
Anthony Calise <anthonycalisee@gmail.com>
2023-04-10 22:02:15 -0400

    o(n) for isAnagram

--- acalise/Arrays & Hashing/isAnagram.ts
+++ acalise/Arrays & Hashing/isAnagram.ts
@@ -2,3 +2,30 @@
 function isAnagram(s: string, t: string): boolean {
   return [...s].sort().toString() === [...t].sort().toString();
 }
+
+// O(n)
+function isAnagram2(s: string, t: string): boolean {
+  if (s.length !== t.length) return false;
+  let sLetterCounts = addLettersToMap(s);
+  let tLetterCounts = addLettersToMap(t);
+  for (let i = 0; i < s.length; i++) {
+    if (sLetterCounts[s[i]] !== tLetterCounts[s[i]]) return false;
+  }
+  return true;
+}
+
+//Helper function for isAnagram2
+function addLettersToMap(str: string): any {
+  let map = {};
+  for (let i = 0; i < str.length; i++) {
+    let letter = str[i];
+    if (typeof map[letter] === "number") {
+      map[letter]++;
+    } else {
+      map[letter] = 1;
+    }
+  }
+  return map;
+}
+
+//
