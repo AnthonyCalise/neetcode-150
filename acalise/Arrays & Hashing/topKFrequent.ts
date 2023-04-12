e660c360a9aa37bef48c134dabf7d8ef0ba92a78
Anthony Calise <anthonycalisee@gmail.com>
2023-04-11 22:32:29 -0400

    topKFrequent

---
+++ acalise/Arrays & Hashing/topKFrequent.ts
@@ -0,0 +1,16 @@
+//O(n log n)
+function topKFrequent(nums: number[], k: number): number[] {
+  let countMap: { [key in number]: number } = {};
+  for (let i = 0; i < nums.length; i++) {
+    countMap[nums[i]] = countMap[nums[i]] ? countMap[nums[i]] + 1 : 1;
+  }
+  let numCounts: number[][] = [];
+  for (const key in countMap) {
+    numCounts.push([parseInt(key), countMap[key]]);
+  }
+  numCounts.sort((a, b) => b[1] - a[1]);
+  const results = numCounts.map((numArr) => {
+    return numArr[0];
+  });
+  return results.slice(0, k);
+}
